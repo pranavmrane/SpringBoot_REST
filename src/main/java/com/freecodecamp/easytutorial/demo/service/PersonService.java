c86ea8450a348e3bf1026973d54e658b8ae9ad6c
pranavmrane <pranavmrane@gmail.com>
2019-09-22 00:48:24 -0400

    First Commit

---
+++ src/main/java/com/freecodecamp/easytutorial/demo/service/PersonService.java
@@ -0,0 +1,42 @@
+package com.freecodecamp.easytutorial.demo.service;
+
+import com.freecodecamp.easytutorial.demo.dao.PersonDAO;
+import com.freecodecamp.easytutorial.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonService(@Qualifier("postgres") PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    public int addPerson(Person person){
+        return personDAO.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople(){
+        return personDAO.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id){
+        return personDAO.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id){
+        return personDAO.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson){
+        return personDAO.updatePersonById(id, newPerson);
+    }
+}
